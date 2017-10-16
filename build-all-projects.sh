206c87ac0d2dca2edc070b6dc7b667b0c3374ebd
Devin Blitzer <devblitzer@gmail.com>
2017-10-15 20:57:41 -0500

    Build success wo tests

---
+++ build-all-projects.sh
@@ -0,0 +1,6 @@
+cd microservice-schedule; ./gradlew clean build; cd ..
+cd microservice-session; ./gradlew clean build; cd ..
+cd microservice-speaker; ./gradlew clean build; cd ..
+cd microservice-vote; ./gradlew clean build; cd ..
+cd web-application; ./gradlew clean build; cd ..
+cd demo-bootstrap; ./gradlew clean build; cd ..
